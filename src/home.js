0a0158595275073d8ab309c942818bef2cf49016
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-18 12:40:17 -0400

    check for changes

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ const welcome = document.createElement('h1');
 const paragraph = document.createElement('p');
 const image = document.createElement('img');
 homeDiv.classList.add('main');
-welcome.innerText = "Welcome to Spice and Dice!"
+welcome.innerText = "Welcome to Spice and Dice!!"
 paragraph.innerText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, a omnis. Omnis voluptatem eveniet repellat? Mollitia non rem nisi ab doloremque architecto asperiores rerum repudiandae adipisci voluptas voluptatum hic laudantium consequatur, enim nulla molestias voluptate sed blanditiis sint nostrum cupiditate. Pariatur repellendus placeat sit minima corrupti consectetur dolores vitae aspernatur!";
 image.src = restaurant;
 homeDiv.append(welcome, paragraph,image);
