8184c7899e6b3a325eecc8d80f7272a9a6bcf673
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-18 12:19:27 -0400

    add menu

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,13 @@
+import drunkenNoodles from './images/drunken_noodles.jpeg'
+
 // Create menu section
 const menuDiv = document.createElement('div');
 const welcome = document.createElement('h1');
 const paragraph = document.createElement('p');
+const drunkenNoodlesImg = document.createElement('img');
 menuDiv.classList.add('main');
 welcome.innerText = "Menu!"
-paragraph.innerText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, a omnis. Omnis voluptatem eveniet repellat? Mollitia non rem nisi ab doloremque architecto asperiores rerum repudiandae adipisci voluptas voluptatum hic laudantium consequatur, enim nulla molestias voluptate sed blanditiis sint nostrum cupiditate. Pariatur repellendus placeat sit minima corrupti consectetur dolores vitae aspernatur!";
-menuDiv.append(welcome, paragraph);
+drunkenNoodlesImg.src = drunkenNoodles;
+menuDiv.append(welcome, drunkenNoodlesImg, paragraph);
 
 export { menuDiv } 
